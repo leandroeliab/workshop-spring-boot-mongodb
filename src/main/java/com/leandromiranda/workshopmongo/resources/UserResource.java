04dd29fe928dde98fbf33bda716353ba445a7618
Leandro Miranda <leandroeliab@hotmail.com>
2025-08-09 23:38:44 -0300

    implementar o DTO no User

--- src/main/java/com/leandromiranda/workshopmongo/resources/UserResource.java
+++ src/main/java/com/leandromiranda/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.leandromiranda.workshopmongo.resources;
 
 import com.leandromiranda.workshopmongo.domain.User;
+import com.leandromiranda.workshopmongo.dto.UserDTO;
 import com.leandromiranda.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/users")
@@ -20,8 +22,9 @@ public class UserResource {
     private UserService service;
 
     @RequestMapping(method=RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(UserDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
